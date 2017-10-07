cef64b44150d67e4554bb0bafbf1773c6c33d919
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2017-10-07 18:14:49 +0200

    get meetups

--- client_src/src/components/Main.js
+++ client_src/src/components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Meetups from './Meetups';
+import MeetupDetails from './MeetupDetails';
 import About from './About';
 
 const Main = () => (
@@ -8,6 +9,7 @@ const Main = () => (
     <Switch>
       <Route exact path='/' component={Meetups} />
       <Route exact path='/about' component={About} />
+      <Route exact path='/meetups/:id' component={MeetupDetails} />
     </Switch>
   </main>
 )
--- client_src/src/components/Meetups.js
+++ client_src/src/components/Meetups.js
@@ -1,10 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import MeetupItem from './MeetupItem';
 
 class Meetups extends Component {
+  constructor() {
+    super();
+    this.state = {
+      meetups: []
+    }
+  }
+
+  // NOTE: Fires when a component renders
+  componentWillMount() {
+    this.getMeetups();
+  }
+
+
+  // NOTE: Get meetups from rest api
+  getMeetups() {
+    axios.get('http://localhost:3000/api/meetups')
+      .then(response => {
+        this.setState({meetups: response.data}, () => {
+          // console.log(this.state);
+        });
+        // console.log(response.data);
+      })
+      .catch(err => console.log(err));
+  }
+
   render(){
+    const meetupItems = this.state.meetups.map((meetup, index) => {
+      return(
+        <MeetupItem key={meetup.id} item={meetup} />
+      )
+    })
+
     return (
       <div>
         <h1>Meetups</h1>
+        <ul className="collection">
+          {meetupItems}
+        </ul>
       </div>
     )
   }
